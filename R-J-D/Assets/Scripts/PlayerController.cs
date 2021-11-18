6ce75e46b06cb1cddb715ce976f70ee6a1433727
DogeTheNuttyMan <jasonrozor@gmail.com>
2021-11-18 15:55:54 +0000

    Changes
    >Spawn Manager Changes (tweaks still required to fix)
    >Player moves left to right aswell now

--- R-J-D/Assets/Scripts/PlayerController.cs
+++ R-J-D/Assets/Scripts/PlayerController.cs
@@ -17,6 +17,36 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Left
+        if (canJump && Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            playerRb.AddForce(Vector3.left * 7, ForceMode.Impulse);
+        }
+
+        if(transform.position.x < -3)
+        {
+            transform.position = new Vector3(-3, transform.position.y, transform.position.z);
+        }
+
+        //Right
+        if (canJump && Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            playerRb.AddForce(Vector3.right * 7, ForceMode.Impulse);
+
+        }
+
+        if (transform.position.x > 3)
+        {
+            transform.position = new Vector3(3 , transform.position.y, transform.position.z);
+        }
+
+        //Z
+        if (transform.position.z != -7)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -7);
+        }
+
+        //Jump
         if (canJump == true && Input.GetKeyDown(KeyCode.UpArrow)) 
         {
             playerRb.AddForce(Vector3.up * 6, ForceMode.Impulse);
